56beb0a4c61b8cd85ae69f46f34b3a20203aef29
Rong Huang <ronghuang0604@berkeley.edu>
2017-05-23 15:30:19 -0700

    classical BS

--- src/com/company/ClassicalBinarySearch.java
+++ src/com/company/ClassicalBinarySearch.java
@@ -4,9 +4,24 @@ package com.company;
  * Created by rong on 5/22/17.
  */
 public class ClassicalBinarySearch {
-//    public int classicalBS(int[] array, int target) {
-//        int left = 0;
-//        int right = array.length - 1;
-//
-//    }
+    public int classicalBS(int[] array, int target) {
+
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == array[mid]) {
+                return mid;
+            }else if (target > array[mid]) {
+                left = mid + 1; // or left = mid;
+            }else if (target < array[mid]){
+                right = mid - 1; // or right = mid;
+            }
+         }
+         return -1;
+    }
 }
---
+++ tests/com/company/ClassicalBinarySearchTest.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by rong on 5/23/17.
+ */
+class ClassicalBinarySearchTest {
+    @Test
+    void classicalBinarySearchTest() {
+        ClassicalBinarySearch test = new ClassicalBinarySearch();
+        int[] array = {1,2,3,4,5};
+        int target = 3;
+        System.out.println(test.classicalBS(array, target));
+    }
+
+    @Test
+    void classicalBinarySearchTest1() {
+        ClassicalBinarySearch test = new ClassicalBinarySearch();
+        int[] array = {1,2,3,4,5};
+        int target = 7;
+        System.out.println(test.classicalBS(array, target));
+    }
+
+    @Test
+    void classicalBinarySearchTest2() {
+        ClassicalBinarySearch test = new ClassicalBinarySearch();
+        int[] array = {1,2,3,4,5};
+        int target = 1;
+        System.out.println(test.classicalBS(array, target));
+    }
+
+    @Test
+    void classicalBinarySearchTest3() {
+        ClassicalBinarySearch test = new ClassicalBinarySearch();
+        int[] array = {1,2,3,4,5};
+        int target = 2;
+        System.out.println(test.classicalBS(array, target));
+    }
+}
